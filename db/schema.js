0a2af2c227472be0ae4f9d7bd583b36fd60c4a85
Christine Movius <christine.movius@gmail.com>
2016-04-11 10:54:14 -0400

    move everything from index.js to schema.js

---
+++ db/schema.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost/reminders');
+
+var db = mongoose.connection;
+
+// will console.log an error if db cannot connect to MongoDB
+db.on('error', function(err){
+  console.log(err);
+});
+// will console.log "database has been connected" if successfully connects
+db.once('open', function() {
+  console.log("database has been connected!");
+
+  // INSERT CODE HERE!
+
+});
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId
+
+var ReminderSchema = new Schema({
+  body: String
+});
+
+var AuthorSchema = new Schema({
+  name: String,
+  reminders: [ReminderSchema]
+});
+
+var ReminderModel = mongoose.model("Reminder", ReminderSchema);
+var AuthorModel = mongoose.model("Author", AuthorSchema);
