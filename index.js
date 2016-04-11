1a68e1059330ee37aa030fe1ba5d244e0a07a317
Christine Movius <christine.movius@gmail.com>
2016-04-11 10:24:49 -0400

    Install mongoose and connect to database.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 var mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost/reminders');
+
+var db = mongoose.connection;
+
+// will console.log an error if db cannot connect to MongoDB
+db.on('error', function(err){
+  console.log(err);
+});
+// will console.log "database has been connected" if successfully connects
+db.once('open', function() {
+  console.log("database has been connected!");
+
+  // INSERT CODE HERE!
+
+});
